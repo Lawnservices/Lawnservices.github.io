2d1f1f86886630510a11672bb005d10bc2b631a5
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-03-21 17:52:44 -0400

    fix-web

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const laguna = document.getElementById('nav');
-document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 260) {
-        nav.style.backgroundColor = 'green';
+// const laguna = document.querySelector('.links');
+ 
+
+// document.addEventListener('scroll', () => {
+//     var scroll_position = window.scrollY;
+//     if (scroll_position > 260) {
+//         laguna.style.color = 'green';
         
-    } else {
-        nav.style.backgroundColor = '#154256'
-    }
-});
+//     } else {
+//         laguna.style.color = '#fff'
+//     }
+// });
 let fechas = document.querySelector('.fecha');
 if(fechas){
     fechas.textContent = new Date().getFullYear();
